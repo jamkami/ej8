c36d8a6726216a1254b397b1ba17350cf8ba900d
Álvaro Martín <33749425+alvaromartingarcia@users.noreply.github.com>
2017-11-24 11:30:33 +0100

    Update calcfact.c

--- calcfact.c
+++ calcfact.c
@@ -4,6 +4,7 @@ int calcfact(int num)
   if (cupon == TRUE){
     //Te ha tocado 100 Millones
     aciertaEuromillon();
+    //Eres millonario
   }
   if (num <= 1) return 1;
   else return (num*calcfact(num-1));
